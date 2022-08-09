2ebca6caf17b39a1e6213664763c8bd7cc87bc9a
Carla Pico <carlaspico@gmail.com>
2022-08-09 14:20:23 -0400

    added a drop down menu

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 function App() {
   const [title, setTitle] = useState("this is the title")
   const [description, setDescription] = useState("your description")
+  const [author, setAuthor] = useState("")
+
   console.log(title)
 
   return (
@@ -21,18 +23,20 @@ function App() {
             <textarea value={description} onChange ={(e)=> {setDescription(e.target.value)}} />
             <h2>{description}</h2>
 
+          {/* here goes the drop down menu bar */}
           <label>Author</label>
-          <select>
-            <option value="todd">Todd</option>
-            <option value="damian">Damian</option>
-            <option value="darie">Darie</option>
-            <option value="jonathan">Jonathan</option>
-            <option value="ludwigson" >Ludwigson</option>
-            <option value="ronnie">Ronnie</option>
-            <option value="noah">Noah</option>
-            <option value=""selected>Choose One </option> 
-            {/* the default would be the first one unless you put selected */}
-          </select>
+            <select value ={author} onChange={(e) => {setAuthor(e.target.value)}}>
+              <option value="todd">Todd</option>
+              <option value="damian">Damian</option>
+              <option value="darie">Darie</option>
+              <option value="jonathan">Jonathan</option>
+              <option value="ludwigson" >Ludwigson</option>
+              <option value="ronnie">Ronnie</option>
+              <option value="noah">Noah</option>
+              <option value=""selected>Choose One </option> 
+              {/* the default would be the first one unless you put selected */}
+            </select>
+            <h2>{author}</h2>
 
       </form>
     </div>
