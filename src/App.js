9ed58e79522812062a3a7c525e539807f0260704
Carla Pico <carlaspico@gmail.com>
2022-08-09 13:57:51 -0400

    added a dropdown menu

--- src/App.js
+++ src/App.js
@@ -10,14 +10,29 @@ function App() {
     <div className="App">
       <form>
           <h1>Comments</h1>
-          
+
+          {/* here goes the title */}
           <label>Title</label>
             <input type="text" value={title} onChange={(e)=> {setTitle (e.target.value)}} />
             <h3>{title}</h3>
 
+          {/* here goes the desciption */}
           <label>Description</label>
-            <textarea value={description} onChange ={(e)=> {setDescription(e.target.value)}}></textarea>
+            <textarea value={description} onChange ={(e)=> {setDescription(e.target.value)}} />
             <h3>{description}</h3>
+
+          <label>Author</label>
+          <select>
+            <option value="todd">Todd</option>
+            <option value="damian">Damian</option>
+            <option value="darie">Darie</option>
+            <option value="jonathan">Jonathan</option>
+            <option value="ludwigson" >Ludwigson</option>
+            <option value="ronnie">Ronnie</option>
+            <option value="noah">Noah</option>
+            <option value=""selected>Choose One </option>
+          </select>
+
       </form>
     </div>
   );
