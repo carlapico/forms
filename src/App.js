1a730bd06af1e34c59c66685b7c0309c41ad174d
Carla Pico <carlaspico@gmail.com>
2022-08-10 15:52:58 -0400

    added Browser Router, Routes, and Route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import {Home, HomeAlone} from './pages/Home';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
 import Header from './components/Header';
+import {Home} from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Footer from './components/Footer';
@@ -9,15 +11,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
-  <>
-    <Home />
-    <HomeAlone/>
+  <BrowserRouter>
+    <Routes> 
+        <Route path='about' element={<h4>about route</h4>}/>
+    </Routes>
+
     <Header />
+    <Home />
     <About />
     <Contact />
     <Footer />
 
-  </>
+  </BrowserRouter>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,21 @@
-import Button from 'react-bootstrap/Button'
+import {Button, Nav} from 'react-bootstrap'
 
-const Header = () => (
-        <>
-            <h3>this is header</h3>
-            <Button variant='dark'>This is bootstrap button </Button>
-        </>
-)
+const Header = () => {
+            {/* <h3>this is header</h3> */}
+            {/* <Button variant='dark'>This is bootstrap button </Button> */}
+            <Nav activeKey = 'home'>
+                    <Nav.Item>
+                        <Nav.Link href="/home">Active</Nav.Link>
+                    </Nav.Item>
+
+                    <Nav.Item>
+                        <Nav.Link eventKey="About-1">About</Nav.Link>
+                    </Nav.Item>
+                    
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-2">Contact</Nav.Link>
+                    </Nav.Item>
+             </Nav>
+}
 
 export default Header
