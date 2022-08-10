33d1395aeb039a0416d8855a2ac864b026a6be95
Carla Pico <carlaspico@gmail.com>
2022-08-10 15:27:34 -0400

    added an html button and imported a button component from react-bootstrap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,10 @@
+import Button from 'react-bootstrap/Button'
+
 const Header = () => (
         <>
             <h3>this is header</h3>
+            <button>this is html</button>
+            <Button>This is bootstrap button </Button>
         </>
 )
 
